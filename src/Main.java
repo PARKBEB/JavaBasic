b0d26225240b262ae8cf6d2288f347ba37aa5ae0
PARKBEB <it.parkbeb@gmail.com>
2023-08-20 23:47:19 +0900

    commit

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("test");
-        System.out.print("test2");
-        System.out.print("test3");
-    }
-}

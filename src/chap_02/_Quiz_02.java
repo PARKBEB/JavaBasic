5b87824729c00df958080f3caa1f7a9accb264f0
PARKBEB <it.parkbeb@gmail.com>
2023-08-24 00:38:47 +0900

    commit

--- src/chap_02/_Quiz_02.java
+++ src/chap_02/_Quiz_02.java
@@ -9,6 +9,8 @@ public class _Quiz_02 {
         System.out.println("탑승객의 키를 입력해주세요.");
         Scanner src= new Scanner(System.in);
         int a = src.nextInt();
+        // 입력 사용이 끝났으니 Scanner 닫음
+        src.close();
 
         String s = (a < 120) ? "탑승 불가능합니다." : "탑승 가능합니다.";
         System.out.println("키가 " + a + "cm이므로 " + s);

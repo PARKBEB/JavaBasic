f12bfb2470d5b4a98ed73c4d2f25d0719c677be5
PARKBEB <it.parkbeb@gmail.com>
2023-08-28 23:52:45 +0900

    commit

--- src/chap_03/_Quiz_03.java
+++ src/chap_03/_Quiz_03.java
@@ -15,6 +15,12 @@ public class _Quiz_03 {
 
         System.out.println(s.substring(i, j) + "*******");
 
+        /*
+        String id = "961206-1234567";
+        System.out.println(id.substring(0, 8)); // 0 위치부터 8 위치 직전까지
+        System.out.println(id.substring(0, id.indexOf("-") + 2)); // 0 위치부터 하이픈 위치 + 2 직전까지
+        */
+
 
     }
 }

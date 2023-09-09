f2ab1e6516230331bc57bae7c20d17e7fe7ac21f
PARKBEB <dmb03140@gmail.com>
2023-09-09 13:50:44 +0900

    commit

---
+++ src/chap_04/_05_For.java
@@ -0,0 +1,12 @@
+package chap_04;
+
+public class _05_For {
+    public static void main(String[] args) {
+        // 반복문 사용 For
+        // for ( 선언 : 조건 : 증강)
+        // 선언 > 조건 > 수행 > 증강 > 조건 ...
+        for (int i = 0; i < 10; i++){
+            System.out.println("환영합니다. 나코입니다. " +i);
+        }
+    }
+}

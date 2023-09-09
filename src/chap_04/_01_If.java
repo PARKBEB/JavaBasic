c40788459488815959117e9fc226d39e881005c8
PARKBEB <dmb03140@gmail.com>
2023-09-09 13:41:53 +0900

    commit

--- src/chap_04/_01_If.java
+++ src/chap_04/_01_If.java
@@ -6,11 +6,11 @@ public class _01_If {
         int hour = 15;
 
         // if문 내에서 하나의 문장을 실행할 때는 {} 생략 가능
-        if(hour < 14)
+        if (hour < 14)
             System.out.println("아이스 아메리카노 +1");
 
         // if문 내에서 2개 이상의 문장을 실행할 때는 {} 생략 불가
-        if(hour < 14) {
+        if (hour < 14) {
             System.out.println("아이스 아메리카노 +1");
             System.out.println("샷추가");
         }
@@ -20,7 +20,7 @@ public class _01_If {
         hour = 10;
         boolean morningCoffee = false;
         // if(hour < 14 && morningCoffee == false){
-        if(hour < 14 && !morningCoffee){
+        if (hour < 14 && !morningCoffee){
             System.out.println("아이스 아메리카노 +1");            
         }
         System.out.println("커피 주문 완료 #2");
@@ -28,7 +28,7 @@ public class _01_If {
         // 오후 2시 이후이거나 모닝 커피를 마신 경우
         hour = 15;
         morningCoffee = true;
-        if(hour >= 14 || !morningCoffee){
+        if (hour >= 14 || !morningCoffee){
             System.out.println("아이스 아메리카노 (디카페인) +1");
         }
         System.out.println("커피 주문 완료 #3");

5fcb7bd51d551416b034af5dc7fbb916a4678326
PARKBEB <it.parkbeb@gmail.com>
2023-09-19 20:23:20 +0900

    Generics, List, Set, Map, Iterator

--- src/chap_09/_Quiz_09.java
+++ src/chap_09/_Quiz_09.java
@@ -4,7 +4,20 @@ import java.util.ArrayList;
 
 public class _Quiz_09 {
     public static void main(String[] args) {
-        Student student = new Student();
+
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(new Student("팬텀은쨩", "파이썬"));
+        students.add(new Student("아델은쨩", "자바"));
+        students.add(new Student("텐텐은쨩", "자바"));
+        students.add(new Student("엔젤릭은쨩", "C"));
+        students.add(new Student("캡틴은쨩", "파이썬"));
+
+        System.out.println("[ 자바 자격증을 보유한 학생 ]");
+        for (Student s : students) {
+            if (s.certificate.equals("자바")) {
+                System.out.println(s.name);
+            }
+        }
 
     }
 }
@@ -12,6 +25,8 @@ public class _Quiz_09 {
 class Student {
     public String name;
     public String certificate;
-
-    ArrayList<String> list = new ArrayList<>();
+    public Student(String name, String certificate) {
+        this.name = name;
+        this.certificate = certificate;
+    }
 }

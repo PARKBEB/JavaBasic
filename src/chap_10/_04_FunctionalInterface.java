6146a742f9a7d87fc6d41384fc74e24ebd41c295
PARKBEB <it.parkbeb@gmail.com>
2023-09-19 21:38:15 +0900

    AnonymousClass, Lambda

---
+++ src/chap_10/_04_FunctionalInterface.java
@@ -0,0 +1,18 @@
+package chap_10;
+
+import chap_10.converter.Convertible;
+import chap_10.converter.KRWConverter;
+
+public class _04_FunctionalInterface {
+    public static void main(String[] args) {
+        KRWConverter converter = new KRWConverter();
+
+       // convertUSD((USD) -> System.out.println(USD + "달러 = " + (USD * 1400) + "원"), 1);
+       Convertible convertible = (USD) -> System.out.println(USD + "달러 = " + (USD * 1400) + "원");
+       convertUSD(convertible, 1);
+    }
+
+    public static void convertUSD(Convertible converter, int USD) {
+        converter.convert(USD);
+    }
+}

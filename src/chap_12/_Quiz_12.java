55d5f2dd772fe845e94a1ff97e28b5650e7ef987
PARKBEB <it.parkbeb@gmail.com>
2023-09-28 11:07:15 +0900

    Tread

--- src/chap_12/_Quiz_12.java
+++ src/chap_12/_Quiz_12.java
@@ -2,6 +2,30 @@ package chap_12;
 
 public class _Quiz_12 {
     public static void main(String[] args) {
+        A a = new A();
+        Thread thread = new Thread(a);
 
+        thread.start();
+        B();
+    }
+    public static void B() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("B 상품 준비 " + i + "/5");
+            if(i == 5) {
+                System.out.println("-- B 상품 준비 완료 --");
+            }
+        }
+    }
+}
+
+class A implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("A 상품 준비 " + i + "/5");
+            if(i == 5) {
+                System.out.println("-- A 상품 준비 완료 --");
+            }
+        }
     }
 }

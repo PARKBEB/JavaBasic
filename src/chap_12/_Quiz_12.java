8df24897ef2835848d89b3682125e132352fae79
PARKBEB <it.parkbeb@gmail.com>
2023-09-28 15:13:11 +0900

    Thread

--- src/chap_12/_Quiz_12.java
+++ src/chap_12/_Quiz_12.java
@@ -3,21 +3,45 @@ package chap_12;
 public class _Quiz_12 {
     public static void main(String[] args) {
         A a = new A();
-        Thread thread = new Thread(a);
-
-        thread.start();
-        B();
-    }
-    public static void B() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("B 상품 준비 " + i + "/5");
-            if(i == 5) {
-                System.out.println("-- B 상품 준비 완료 --");
+        A b = new A() {
+            public void run() {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("B 상품 준비 " + i + "/5");
+                if (i == 5) {
+                    System.out.println("-- B 상품 준비 완료 --");
+                }
             }
+            }
+        };
+
+        Thread thread = new Thread(a);
+        Thread thread1 = new Thread(b);
+
+        thread1.start();
+        thread.start();
+
+        try {
+            thread.join();  // thread가 끝날 때까지 대기
+            thread1.join(); // thread1이 끝날 때까지 대기
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
         }
+
+        A set = new A() {
+            public void run() {
+                for (int i = 1; i <= 5; i++) {
+                    System.out.println("세트 상품 포장" + i + "/5");
+                    if (i == 5) {
+                        System.out.println("== 세트 상품 포장 완료 ==");
+                    }
+                }
+            }
+        };
+
+        Thread thread2 = new Thread(set);
+        thread2.start();
     }
 }
-
 class A implements Runnable {
     @Override
     public void run() {
